37d85c3ef15fbc10b5d9bfb9124d47d864a5ada5
aldcejam <aldcejamj923@gmail.com>
2023-12-04 17:48:38 -0300

    first commit

---
+++ dist/test.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.helloworld = void 0;
+function helloworld() {
+    return 'Hello World!';
+}
+exports.helloworld = helloworld;
